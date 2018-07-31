564150e54ab6495fd22cf4bc05a7fca46e18e97f
gulya <gulyaluehrs@gmail.com>
2018-07-31 17:15:47 +0200

    commit 1

---
+++ Desktop/helloQA/src/Main.java
@@ -0,0 +1,12 @@
+import sun.nio.cs.ext.MacThai;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 42;
+        int b = 42;
+
+
+        System.out.println("Hello QA");
+    }
+}
